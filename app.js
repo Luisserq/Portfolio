c7ec9cc8629ec34064a5b047d9e50b1b0c8f39d0
Luisserq <78772653+Luisserq@users.noreply.github.com>
2021-12-16 20:15:02 -0800

    port

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const mobile = document.querySelector(".menu-toggle");
+const menuLinks = document.querySelector(".menu");
+
+mobile.addEventListener("click", function (){
+    mobile.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+});
